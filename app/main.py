0b26269a381e31ad041f7a0cc18267cdf458a2bc
Aditya Shinde <aditya.manoj.shinde@gmail.com>
2026-04-18 22:56:44 +0530

    updated: app/main.py; purpose: add reload for debugging;

--- app/main.py
+++ app/main.py
@@ -12,4 +12,4 @@ def root():
 
 if __name__ == "__main__":
     # Use reload=True only in development for auto-reload on code changes
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000) 
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True) 
